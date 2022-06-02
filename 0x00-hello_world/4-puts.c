49f6e50a9fd4846f9a37951079670c065dea96ba
lonjie254 <kiarie.longinus@gmail.com>
2022-06-02 16:24:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - entry point of the program
  *
- * return : Always 0 (success)
+ *Description - prints exactly
+ *
+ *Return: Always 0
  */
 int main(void)
 {
