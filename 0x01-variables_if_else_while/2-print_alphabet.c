0d9bd13db9df561c92abfba1bc96611bb0bbeb19
lonjie254 <kiarie.longinus@gmail.com>
2022-06-03 04:36:50 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ *main - entry point
+ *
+ *description - A-Z characters in lowercase
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+for (int x = 'A'; x <= 'Z'; x++)
+{
+int lower_x = tolower(x);
+putchar(lower_x);
+}
+return (0);
+}
