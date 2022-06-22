496c5da13e650c6040d66d0f7538d9735ed62c83
lonjie254 <kiarie.longinus@gmail.com>
2022-06-22 11:30:12 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_printer - print a string from end to start in recursion
+ * @s: a char
+ * @n: a shift
+ * Return: void
+ */
+
+void rev_printer(char *s, int n)
+{
+	if (n < 0)
+		return;
+	_putchar(*(s + n));
+	rev_printer(s, n - 1);
+}
+/**
+ * _print_rev_recursion - recursively print a string from end to start
+ * @s: a char
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	int n = strlen(s);
+
+	rev_printer(s, n - 1);
+}
