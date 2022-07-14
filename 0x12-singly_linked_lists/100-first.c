5484c242f33d487aa726eb0dc686435a1831a700
lonjie254 <kiarie.longinus@gmail.com>
2022-07-14 09:51:28 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include "stdio.h"
+/**
+ * myFuncode - this function runs before main
+ */
+void myFuncode(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
