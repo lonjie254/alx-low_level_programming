eb9ad609de7eb6cb3e5757379f56555baf2ddaa4
lonjie254 <105755934+lonjie254@users.noreply.github.com>
2022-09-24 22:07:44 +0300

    Update 0-hash_table_create.c
    new

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,18 +10,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (!size)
 		return (NULL);
 
+	ht = calloc(1, sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (!ht->array)
+	{
+		free(ht);
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	}
 
 	return (ht);
 }
