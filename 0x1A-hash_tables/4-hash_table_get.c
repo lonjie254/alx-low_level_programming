966f5442f414cdb849e39e15847281b3bd6240a0
lonjie254 <105755934+lonjie254@users.noreply.github.com>
2022-09-27 13:21:56 +0300

    Update 4-hash_table_get.c
    new 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,22 +7,24 @@
  *@key: the key you are looking for
  *
  *Return: the value associated with the element else NULL
- */
+ **/
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *nodo;
-unsigned long int index;
+	hash_node_t *nodo;
+	unsigned long int index;
 
-if (key == NULL || ht == NULL)
-return (NULL);
+	if (key == NULL || ht == NULL)
+		return (NULL);
 
-index = key_index((const unsigned char *)key, ht->size);
-nodo = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	nodo = ht->array[index];
 
-for (; nodo != NULL; nodo = nodo->next)
-{
-if (strcmp(nodo->key, key) == 0)
-return (nodo->value);
-}
-return (NULL);
+	for (; nodo != NULL; nodo = nodo->next)
+	{
+		if (strcmp(nodo->key, key) == 0)
+			return (nodo->value);
+	}
+
+	return (NULL);
 }
